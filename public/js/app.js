acf7691ddf116c0d313625721d2ec7b08116f0d0
Alexander Inozemtsev <2275399@gmail.com>
2020-05-04 11:38:45 +0300

    voting app finalize

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,28 @@
-
 class ProductList extends React.Component {
+
+  state = {
+    products: [],
+  };
+  componentDidMount() {
+    this.setState({ products: Seed.products });
+  }
+
+  handleProductUpVote = (productId) => {
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    })
+    this.setState({
+      products: nextProducts,
+    })
+  }
   render() {
-    const products = Seed.products.sort((a, b) => (
+    const products = this.state.products.sort((a, b) => (
       b.votes - a.votes
     ));
 
@@ -15,6 +36,7 @@ class ProductList extends React.Component {
         votes={product.votes}
         submitterAvatarUrl={product.submitterAvatarUrl}
         productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
       />
     ))
     return (
@@ -27,18 +49,22 @@ class ProductList extends React.Component {
 }
 
 class Product extends React.Component {
+  handleUpVote = () => (
+
+    this.props.onVote(this.props.id)
+  )
+
   render() {
     return (
       <div className='item'>
         <div className='image'>
           <img
-            // width="100" height="100"
             src={this.props.productImageUrl} />
         </div>
         <div className='middle aligned content'>
           <div className='header'>
-            <a>
-              <i className='large caret up icon' width="50" height="30" />
+            <a onClick={this.handleUpVote}>
+              <i className='large caret up icon'></i>
             </a>
             {this.props.votes}
           </div>
@@ -55,7 +81,6 @@ class Product extends React.Component {
             <img
               className='ui avatar image'
               src={this.props.submitterAvatarUrl}
-              width={30} height="30" />
             />
           </div>
         </div>
